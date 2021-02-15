6cb19d707bc81cd423a97ecc0e417d0642fcf5af
codabu <57690093+codabu@users.noreply.github.com>
2021-02-15 14:13:18 -0600

    added fles

---
+++ Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "")
+        {
+            string contentString = "Counting down:\n";
+            for (int i = start; i >= end; i--)
+            {
+                contentString += i + "\n";
+            }
+            return Content(contentString + message);
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult Dummy1()
+        {
+            return View();
+        }
+
+        public IActionResult Dummy2(string id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+
+        }
+
+        [Route("Retail/[action]/{id?}")]
+        public IActionResult Dummy3()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ch06Ex1RoutingPractice/RoutingPractice/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RoutingPractice
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "customRoute",
+                    pattern: "{controller=Home}/{action=Dummy2}/{id?}");
+
+            });
+        }
+    }
+}
